6f7e17d654733783a2a407a4f047e37d8aab2b5f
Michael Kang <michael.kang@fdmgroup.com>
2021-02-15 13:28:00 -0500

    mark notification as hidden public func

--- TikTok/Managers/DatabaseManager.swift
+++ TikTok/Managers/DatabaseManager.swift
@@ -100,6 +100,10 @@ final class DatabaseManager {
         }
     }
     
+    public func markNotificationAsHidden(completion: @escaping ([Notification]) -> Void) {
+        completion(Notification.mockData())
+    }
+    
     
     public func getUsername(for email: String, completion: @escaping (String?) -> Void) {
         database.child("users").observeSingleEvent(of: .value) { (snapshot) in
